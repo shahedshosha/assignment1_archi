5323eccaafcafd033c6959cbadf2c272aafc7a08
shahedshosha <102873895+shahedshosha@users.noreply.github.com>
2022-05-15 23:39:36 +0300

    Add project files.

---
+++ assignment1_archi/GUI/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace assignment1_archi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStoreList = new System.Windows.Forms.Label();
+            this.dataGridViewStoreManage = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtStorePhone = new System.Windows.Forms.TextBox();
+            this.lblStorePhone = new System.Windows.Forms.Label();
+            this.txtStoreName = new System.Windows.Forms.TextBox();
+            this.lblStoreName = new System.Windows.Forms.Label();
+            this.lblStoreID = new System.Windows.Forms.Label();
+            this.txtStoreID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStoreManage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStoreList
+            // 
+            this.lblStoreList.AutoSize = true;
+            this.lblStoreList.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStoreList.Location = new System.Drawing.Point(710, 30);
+            this.lblStoreList.Name = "lblStoreList";
+            this.lblStoreList.Size = new System.Drawing.Size(124, 29);
+            this.lblStoreList.TabIndex = 101;
+            this.lblStoreList.Text = "Store List";
+            // 
+            // dataGridViewStoreManage
+            // 
+            this.dataGridViewStoreManage.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewStoreManage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStoreManage.Location = new System.Drawing.Point(408, 72);
+            this.dataGridViewStoreManage.Name = "dataGridViewStoreManage";
+            this.dataGridViewStoreManage.RowHeadersWidth = 51;
+            this.dataGridViewStoreManage.RowTemplate.Height = 24;
+            this.dataGridViewStoreManage.Size = new System.Drawing.Size(701, 395);
+            this.dataGridViewStoreManage.TabIndex = 100;
+            this.dataGridViewStoreManage.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewStoreManage_CellClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(246, 302);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(102, 54);
+            this.btnDelete.TabIndex = 98;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(138, 302);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(102, 54);
+            this.btnUpdate.TabIndex = 97;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(30, 302);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(102, 54);
+            this.btnAdd.TabIndex = 96;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtStorePhone
+            // 
+            this.txtStorePhone.Location = new System.Drawing.Point(30, 194);
+            this.txtStorePhone.Name = "txtStorePhone";
+            this.txtStorePhone.Size = new System.Drawing.Size(184, 22);
+            this.txtStorePhone.TabIndex = 95;
+            // 
+            // lblStorePhone
+            // 
+            this.lblStorePhone.AutoSize = true;
+            this.lblStorePhone.Location = new System.Drawing.Point(27, 168);
+            this.lblStorePhone.Name = "lblStorePhone";
+            this.lblStorePhone.Size = new System.Drawing.Size(87, 17);
+            this.lblStorePhone.TabIndex = 94;
+            this.lblStorePhone.Text = "Store Phone";
+            // 
+            // txtStoreName
+            // 
+            this.txtStoreName.Location = new System.Drawing.Point(30, 131);
+            this.txtStoreName.Name = "txtStoreName";
+            this.txtStoreName.Size = new System.Drawing.Size(184, 22);
+            this.txtStoreName.TabIndex = 93;
+            // 
+            // lblStoreName
+            // 
+            this.lblStoreName.AutoSize = true;
+            this.lblStoreName.Location = new System.Drawing.Point(27, 105);
+            this.lblStoreName.Name = "lblStoreName";
+            this.lblStoreName.Size = new System.Drawing.Size(83, 17);
+            this.lblStoreName.TabIndex = 92;
+            this.lblStoreName.Text = "Store Name";
+            // 
+            // lblStoreID
+            // 
+            this.lblStoreID.AutoSize = true;
+            this.lblStoreID.Location = new System.Drawing.Point(27, 40);
+            this.lblStoreID.Name = "lblStoreID";
+            this.lblStoreID.Size = new System.Drawing.Size(59, 17);
+            this.lblStoreID.TabIndex = 90;
+            this.lblStoreID.Text = "Store ID";
+            // 
+            // txtStoreID
+            // 
+            this.txtStoreID.Location = new System.Drawing.Point(30, 66);
+            this.txtStoreID.Name = "txtStoreID";
+            this.txtStoreID.Size = new System.Drawing.Size(184, 22);
+            this.txtStoreID.TabIndex = 91;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1139, 492);
+            this.Controls.Add(this.lblStoreList);
+            this.Controls.Add(this.dataGridViewStoreManage);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtStorePhone);
+            this.Controls.Add(this.lblStorePhone);
+            this.Controls.Add(this.txtStoreName);
+            this.Controls.Add(this.lblStoreName);
+            this.Controls.Add(this.lblStoreID);
+            this.Controls.Add(this.txtStoreID);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStoreManage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStoreList;
+        private System.Windows.Forms.DataGridView dataGridViewStoreManage;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtStorePhone;
+        private System.Windows.Forms.Label lblStorePhone;
+        private System.Windows.Forms.TextBox txtStoreName;
+        private System.Windows.Forms.Label lblStoreName;
+        private System.Windows.Forms.Label lblStoreID;
+        private System.Windows.Forms.TextBox txtStoreID;
+    }
+}
+
